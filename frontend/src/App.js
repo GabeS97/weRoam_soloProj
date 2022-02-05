2783a96248a904b22072849c42bb7ad41a89f562
Gabriel Sitorus <sitorusgg@gmail.com>
2022-02-06 06:11:27 +0700

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import { Switch } from 'react-router-dom'
+import Navigation from "./components/Navigation";
+import { useDispatch } from 'react-redux'
+import * as sessionActions from "./store/session";
+
+
+
+function App() {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
+  }, [dispatch]);
+
+
+  return isLoaded && (
+    <>
+      <Navigation isLoaded={isLoaded} />
+      <Switch>
+      </Switch>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Navigation/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import ProfileButton from './ProfileButton';
+import LoginFormModal from '../LoginFormModal';
+import SignupFormModal from '../SignupFormModal'
+import './Navigation.css';
+
+function Navigation({ isLoaded }){
+  const sessionUser = useSelector(state => state.session.user);
+
+  let sessionLinks;
+  if (sessionUser) {
+    sessionLinks = (
+      <ProfileButton user={sessionUser} />
+    );
+  } else {
+    sessionLinks = (
+      <>
+        <LoginFormModal />
+        <SignupFormModal />
+      </>
+    );
+  }
+
+  return (
+      <nav className='navBar'>
+        <ul>
+            <li>
+                <NavLink exact to="/">Home</NavLink>
+                {isLoaded && sessionLinks}
+            </li>
+        </ul>
+      </nav>  
+  );
+}
+
+export default Navigation;
